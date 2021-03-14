ed1ea07c18f5ee5d2516d81edaca43997609e1f7
Aigul <sharipova.aig@gmail.com>
2021-03-14 16:59:48 -0400

    Add ejs and home page

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-// Load express
 const express = require('express');
-
-// Create our express app
 const app = express();
 
-// Define a "root" route directly on app
-// Tomorrow, we'll use best practice routing
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
   res.send('<h1>Hello SEI!</h1>');
 });
 
 app.get('/home', function(req, res) {
-  res.send('<h1>Home Page!</h1>');
+  //res.send('<h1>Home Page!</h1>');
+  res.render('home');
 });
 
 app.get('/api', function(req, res) {
   res.json({ fruits: ['oranges', 'bananas', 'mango'] });
 });
-// Tell the app to listen on port 3000
-// for HTTP requests from clients
+
 app.listen(3000, function () {
   console.log('Listening on port 3000');
 });
